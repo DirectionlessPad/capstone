1033220fc513c16fca02e5c33333bb547dddef2a
DirectionlessPad <patrickmoore937@gmail.com>
2024-04-23 13:48:22 +0100

    Login, register, logout, layout setup, chess pieces images downloaded

--- chessapp/djangochess/models.py
+++ chessapp/djangochess/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    # num_wins = models.IntegerField()
+    # num_draws = models.IntegerField()
+    # num_losses = models.IntegerField()
+    # rating = models.DecimalField()
+    pass
